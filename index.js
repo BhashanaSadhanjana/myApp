5c3ae78de981b26c42066234bad4eb6f159a361e
Bhashana Sadanjana <bhashana1999@gmail.com>
2021-07-11 18:38:49 +0530

    added the baseroute to the index

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 // initialize routes
 // middle ware 02
 app.use('/api',require('./routes/api'));
+app.use('/api',require('./routes/baseRoute'));
 
 //error handling middleware
 app.use(function (err,req,res,next) {
